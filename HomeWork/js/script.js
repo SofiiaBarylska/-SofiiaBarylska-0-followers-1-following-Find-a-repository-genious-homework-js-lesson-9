d9edf90a1ab4cbc3377ecd27c2d3b016836ee80a
Sofiia Barylska <sofiia.barylska@smile-ukraine.com>
2024-07-11 11:04:07 +0300

    answers for homework 9

---
+++ HomeWork/js/script.js
@@ -0,0 +1,25 @@
+//-----1----
+//Напишіть такий JavaScript, щоб після натискання на кнопку button, елемент
+//<div id="text"> зникав.
+//-----2----
+//Напишіть такий код, щоб після натискання на кнопку, вона зникала.
+//-----3----
+//Створіть дерево, яке показує/приховує дочірні вузли при кліці
+
+
+/* const btn = document.querySelector("#hider")
+const text = document.querySelector("#text")
+
+btn.addEventListener('click', handleClick);
+
+function handleClick(event) {
+    text.hidden = true;
+} */
+
+/* const btn = document.querySelector("#hider")
+
+btn.addEventListener('click', handleClick);
+
+function handleClick(event) {
+    btn.hidden = true;
+} */
